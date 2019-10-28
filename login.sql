94bf872ffd5c8d06a141414502b2e2e1c71684ac
Stud430 <54825589+Stud430@users.noreply.github.com>
2019-10-28 15:20:35 -0300

    Add files via upload

--- login.sql
+++ login.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 17-Out-2019 às 01:19
+-- Generation Time: 28-Out-2019 às 19:10
 -- Versão do servidor: 5.7.17
--- PHP Version: 5.6.30
+-- PHP Version: 7.1.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,19 +30,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `login` (
   `id` int(11) NOT NULL,
-  `nome` varchar(30) NOT NULL,
-  `usuario` varchar(10) NOT NULL,
-  `senha` varchar(10) NOT NULL
+  `nome_completo` varchar(30) NOT NULL,
+  `idade` int(3) DEFAULT NULL,
+  `email` varchar(40) NOT NULL,
+  `usuario` varchar(10) DEFAULT NULL,
+  `senha` varchar(15) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `login`
 --
 
-INSERT INTO `login` (`id`, `nome`, `usuario`, `senha`) VALUES
-(12, 'C1clon3 Sword', 'ts3Sword', 'ciclope'),
-(11, 'Laura Caixao', 'LauraSims', 'ts3Laura'),
-(10, 'Paula Costa', 'pCosta', '123');
+INSERT INTO `login` (`id`, `nome_completo`, `idade`, `email`, `usuario`, `senha`) VALUES
+(1, 'Fred Flintstone', 30, 'bedrock.fred@gmail.com', 'bR0ckFr3d', 'wilmapebbles'),
+(2, 'Wilma Flintstone', 29, 'wilmafred@gmail.com', 'W1lm4', 'Pebbles1'),
+(3, 'Pebbles Flintstone', 1, 'pebbles001@gmail.com', 'M3Pebbles', 'babyP3bbles'),
+(4, 'Lola LooneyTunes', 4, 'tuneslola@yahoo.com.br', 'LooTunes', 'lola10'),
+(5, 'Frajola LooneyTunes', 5, 'frajtunes@ig.com.br', 'looneyF', 'frajolakid');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +66,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
